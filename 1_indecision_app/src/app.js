2abebfcdc253f28b333143c14ec6e2c4eec5cad3
heunghari@naver.com <heunghari@naver.com>
2020-01-11 22:13:37 +0900

    Setting up Babel

---
+++ 1_indecision_app/src/app.js
@@ -0,0 +1,9 @@
+// src 아래에 있는 이 app.js 파일이 babel을 통해서 compile된 다음에
+// 그 compile된 내용이 scripts 안에 있는 app.js파일로 전달될것임
+console.log('App.js is running!');
+
+// JSX - Javascript XML
+var template  = <h1> Indecision App </h1>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot); // render 'template' contents in 'appRoot' element

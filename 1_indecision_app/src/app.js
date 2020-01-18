9c3f7fffc25176d6b1f7cc811f0418fab07ed43f
heunghari@naver.com <heunghari@naver.com>
2020-01-19 03:38:58 +0900

    Arrays in JSX

--- 1_indecision_app/src/app.js
+++ 1_indecision_app/src/app.js
@@ -39,8 +39,11 @@ const renderTemplate = () => {
             <p> { app.options.length } </p>
             <button onClick={removeAll}> Remove All </button>
             <ol>
-                <li>Item one</li>
-                <li>Item two</li>
+                {
+                    app.options.map( (option) => {
+                        return <li key={option}> {option} </li>;
+                    })
+                }
             </ol>
             <form onSubmit={onFormSubmit}>
                 <input type="text" name="option" />
